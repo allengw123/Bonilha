d2734a7c13969af64f13b022979778c5c015b73e
allengw123 <67129544+allengw123@users.noreply.github.com>
2021-10-11 16:44:54 -0400

    Addition of vbm

--- Scripts/VBM.m
+++ Scripts/VBM.m
@@ -29,7 +29,6 @@ cd(PatientData)
 
 SmoothThres=fullfile(PatientData,'thres');
 addpath(genpath(SmoothThres));
-cnn_output = 'F:\CNN output';
 
 matter='GM';
 
@@ -55,23 +54,30 @@ ADNI_Alz_info=readtable(fullfile(PatientData,'ADNI_Alz_info.csv'));
 
 %% get volume data
 
-[Alz_GM, Alz_GM_names] = get_volume_data(Alzfiles);
+[Alz_GM, Alz_GM_names,template_nii] = get_volume_data(Alzfiles);
 [TLE_GM, TLE_GM_names] = get_volume_data(tlefiles);
 [Control_GM, Control_GM_names] = get_volume_data(controlfiles);
  
-%% compare volumes
+%% compare volumes (t-test/bonferroni)
+ttest_savepath=fullfile(save_path,'ttest');
+mkdir(ttest_savepath);
+
 [P, T] = compare_volumes(Alz_GM, TLE_GM,...
-    controlfiles{1},save_path,'Alz vs TLE');
+    controlfiles{1},ttest_savepath,'Alz vs TLE');
 
 [P, T] = compare_volumes(Alz_GM, Control_GM,...
-    controlfiles{1},save_path,'Alz vs Control');
+    controlfiles{1},ttest_savepath,'Alz vs Control');
 
 [P, T] = compare_volumes(TLE_GM, Control_GM,...
-    controlfiles{1},save_path,'TLE vs Control');
+    controlfiles{1},ttest_savepath,'TLE vs Control');
 
-cd(save_path)
+cd(ttest_savepath)
 %% Fitlm
 
+fitlm_savepath=fullfile(save_path,'fitlm');
+mkdir(fitlm_savepath)
+
+% Organize and vectorize Alz data/age
 count=1;
 for i=1:size(Alz_GM,4)
     tempname=extractBetween(Alz_GM_names(i),'_I','.nii');
@@ -87,6 +93,7 @@ for i=1:size(Alz_GM,4)
     end
 end
 
+% Organize and vectorize TLE data/age
 count=1;
 for i=1:size(TLE_GM,4)
     tempname=extractBetween(TLE_GM_names(i),'_GM_','.nii');
@@ -102,6 +109,7 @@ for i=1:size(TLE_GM,4)
     end
 end
 
+% Organize and vectorize Control data/age
 count=1;
 for i=1:size(Control_GM,4)
     tempname=extractBetween(Control_GM_names(i),'GM_','.nii');
@@ -122,26 +130,102 @@ for i=1:size(Control_GM,4)
     end
 end
 
-per=nchoosek({'Alz','TLE','Control'},2);
+% Create local pool
+n = parcluster('local');
+nworkers = n.NumWorkers;
 
+
+% Run linear model
+per=nchoosek({'Alz','TLE','Control'},2);
 for p=1:size(per,1)
-    comp=[per{p,1} '(0) vs ' per{p,2},'(1)'];
+    
+    comp{p}=[per{p,1} '(0) vs ' per{p,2},'(1)'];
+    disp(['Running comparison ',comp{p}])
+    
+    % Organize image/age data
     stacked=eval(['[',per{p,1},'_GM_vec ; ',per{p,2},'_GM_vec]']);
-    y=[zeros(size(eval([per{p,1},'_GM_vec']),1),1) ;ones(size(eval([per{p,2},'_GM_vec']),1),1)];
     age_stacked=[eval([per{p,1},'_GM_age']);eval([per{p,2},'_GM_age'])];
-    for vox=1:size(stacked,2)
-        mdl{p,vox}=fitlm([age_stacked stacked(:,vox)], y);
+    additional=ones(size(stacked,1),(size(stacked,1)*nworkers)-(rem(numel(stacked),(size(stacked,1)*nworkers)))/size(stacked,1));
+    stacked_reshape=reshape([stacked additional],size(stacked,1),[],12);
+    
+    % Label data
+    y=[zeros(size(eval([per{p,1},'_GM_vec']),1),1) ;ones(size(eval([per{p,2},'_GM_vec']),1),1)];
+    
+    % Run parallel pool fitlm calculation
+    tempx1=[];
+    tempx2=[];
+    tic
+    spmd
+        warning('off','all')
+        for i=1:size(stacked_reshape,2)
+            x=[age_stacked stacked_reshape(:,i,labindex)];
+            mdl=fitlm(x,y);
+            
+            % save pval
+            tempx1.pval(i,1)=mdl.Coefficients.pValue(2);
+            tempx2.pval(i,1)=mdl.Coefficients.pValue(3);
+
+            % save tStat
+            tempx1.tStat(i,1)=mdl.Coefficients.tStat(2);
+            tempx2.tStat(i,1)=mdl.Coefficients.tStat(3);
+        end
     end
+    toc
+    
+    % Rearrange data from parallel pool
+    tempx1_pval=[];
+    tempx1_tStat=[];
+    tempx2_pval=[];
+    tempx2_tStat=[];
+    for i=1:nworkers
+        % x1
+        temp=tempx1{i};
+        tempx1_pval=[tempx1_pval temp.pval'];
+        tempx1_tStat=[tempx1_tStat temp.tStat'];
+        
+        %x2
+        temp=tempx2{i};
+        tempx2_pval=[tempx2_pval temp.pval'];
+        tempx2_tStat=[tempx2_tStat temp.tStat'];
+    end
+    
+    % Reshape vectorized data back into 3D-image
+    x1{p}.pval=reshape(tempx1_pval(1:end-size(additional,2)),size(C));
+    x1{p}.tStat=reshape(tempx1_tStat(1:end-size(additional,2)),size(C));
+    
+    x2{p}.pval=reshape(tempx2_pval(1:end-size(additional,2)),size(C));
+    x2{p}.tStat=reshape(tempx2_tStat(1:end-size(additional,2)),size(C));
+    
+    % Save x1 (age) as nifti
+    x1_nii_pval=template_nii;
+    x1_nii_pval.img=x1{p}.pval;
+    save_nii(x1_nii_pval,fullfile(fitlm_savepath,[comp{p},' --AGE-- pval.nii']));
+    
+    x1_nii_tStat=template_nii;
+    x1_nii_tStat.img=x1{p}.tStat;
+    save_nii(x1_nii_tStat,fullfile(fitlm_savepath,[comp{p},' --AGE-- tStat.nii']));
+    
+    % Save x2 (voxel intensity) as nifti
+    x2_nii_pval=template_nii;
+    x2_nii_pval.img=x2{p}.tStat;
+    save_nii(x2_nii_pval,fullfile(fitlm_savepath,[comp{p},' --VOX-- pval.nii']));
+    
+    x2_nii_tStat=template_nii;
+    x2_nii_tStat.img=x2{p}.tStat;
+    save_nii(x2_nii_tStat,fullfile(fitlm_savepath,[comp{p},' --VOX-- tStat.nii']));
 end
+
+save(fullfile(fitlm_savepath,'fitlm_output.mat'),'x1','x2','comp')
+
 
 %% supporting functions
 
-function [X, X_names] = get_volume_data(ff)
+function [X, X_names,N] = get_volume_data(ff)
     count = 1;
     for i = 1:numel(ff)
         N = load_nii(ff{i});
         X(:,:,:,count) = N.img;
-        X_names{count,1} =ff{i};
+        X_names{count,1} =ff{i}; 
         count = count + 1;
     end
 end
