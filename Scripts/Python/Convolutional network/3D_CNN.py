4242db603eaf1b2f6582dc62fc616c8af4ce24d4
allengw123 <67129544+allengw123@users.noreply.github.com>
2022-02-16 09:47:39 -0500

    Update 3D_CNN.py

--- Scripts/Python/Convolutional network/3D_CNN.py
+++ Scripts/Python/Convolutional network/3D_CNN.py
@@ -36,7 +36,6 @@ def NormalizeData(data):
 def PreprocImg(input):
     imgs = [nib.load(file).get_fdata().astype(np.float32) for file in input]
     imgs = NormalizeData(np.stack(imgs,axis=3))
-    imgs = np.expand_dims(imgs,axis=4)
     return imgs
 
 
@@ -67,7 +66,8 @@ class CNNinput:
         self.testingImg = PreprocImg(self.testingFiles)
 
 # Build a 3D convolutional neural network model
-def get_model(width, height, depth):
+def get_model(width=128, height=128, depth=64):
+    """Build a 3D convolutional neural network model."""
 
     inputs = keras.Input((width, height, depth, 1))
 
@@ -96,6 +96,7 @@ def get_model(width, height, depth):
     # Define the model.
     model = keras.Model(inputs, outputs, name="3dcnn")
     return model
+
 
 
 def rotate(volume):
@@ -127,6 +128,7 @@ def validation_preprocessing(volume, label):
     volume = tf.expand_dims(volume, axis=3)
 
     return volume, label
+
 #%%
 # eliminate error notifications
 os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
@@ -152,44 +154,57 @@ for i in range(iterations):
     
     # Pepare x y data
     train_images = np.concatenate(
-        [LTLEinput.testingImg, RTLEinput.testingImg, Healthyinput.testingImg],axis=3).transpose((3,0,1,2,4))
+        [LTLEinput.testingImg, RTLEinput.testingImg, Healthyinput.testingImg],axis=3).transpose((3,0,1,2))
     train_labels = np.concatenate([np.zeros(LTLEinput.testingImg.shape[3]),  np.ones(RTLEinput.testingImg.shape[3]), np.ones(
         Healthyinput.testingImg.shape[3])*2])
     
     validation_images = np.concatenate(
-        [LTLEinput.validationImg, RTLEinput.validationImg, Healthyinput.validationImg],axis=3).transpose((3,0,1,2,4))
+        [LTLEinput.validationImg, RTLEinput.validationImg, Healthyinput.validationImg],axis=3).transpose((3,0,1,2))
     validation_labels = np.concatenate([np.zeros(LTLEinput.validationImg.shape[3]), np.ones(RTLEinput.validationImg.shape[3]), np.ones(
         Healthyinput.validationImg.shape[3])*2])
     
     test_images = np.concatenate(
-        [LTLEinput.testingImg, RTLEinput.testingImg, Healthyinput.testingImg],axis=3).transpose((3,0,1,2,4))
+        [LTLEinput.testingImg, RTLEinput.testingImg, Healthyinput.testingImg],axis=3).transpose((3,0,1,2))
     test_labels = np.concatenate([np.zeros(LTLEinput.testingImg.shape[3]), np.ones(RTLEinput.testingImg.shape[3]), np.ones(
         Healthyinput.testingImg.shape[3])*2])
     
     # Define data loaders
     train_loader = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
     validation_loader = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
-    
+
     # Preproc dataset
     batch_size = 2
 
-    train_dataset = train_loader.shuffle(len(train_labels),reshuffle_each_iteration=True).map(train_preprocessing).batch(batch_size).prefetch(1)
-    validation_dataset = validation_loader.shuffle(len(validation_labels),reshuffle_each_iteration=True).map(validation_preprocessing).batch(batch_size).prefetch(1)
+    train_dataset = train_loader.shuffle(len(train_labels),reshuffle_each_iteration=True).map(train_preprocessing).batch(batch_size).prefetch(2)
+    validation_dataset = validation_loader.shuffle(len(validation_labels),reshuffle_each_iteration=True).map(validation_preprocessing).batch(batch_size).prefetch(2)
+    
+
+    
+    data = train_dataset.take(1)
+    images, labels = list(data)[0]
+    images = images.numpy()
+    image = images[0]
+    print("Dimension of the CT scan is:", image.shape)
+    plt.imshow(np.squeeze(image[:, :, 20]), cmap="gray")
     
     # Prepare Model
     model=get_model(113,137,113)
     
-    loss = keras.losses.SparseCategoricalCrossentropy()
-    optim = keras.optimizers.SGD(learning_rate=0.01, momentum=0.09)
-    metrics = ["accuracy"]
-    
-    model.compile(optimizer=optim, loss=loss, metrics=metrics)
+    # Compile model.
+    initial_learning_rate = 0.0001
+    lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True)
+    model.compile(
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
+        metrics=["acc"],)
+
     epoch = 30
     
     ############ Run CNN model
     
     # Train Model
-    model.fit(train_dataset, epochs=epoch, batch_size=batch_size, verbose=2,validation_data=validation_dataset,shuffle=True)
+    model.fit(train_dataset, epochs=epoch, verbose=2,validation_data=validation_dataset,shuffle=True)
     
     # Test Model
     accuracy.append(model.evaluate(test_images, test_labels, batch_size=batch_size, verbose=2))
